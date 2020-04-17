34a1498887900426bd285d8ff33c27fadc66dbf5
Lahr Max <52128659+Lahma712@users.noreply.github.com>
2020-04-17 16:30:29 +0200

    Update Image-To-PDF.py

--- Image-To-PDF.py
+++ Image-To-PDF.py
@@ -7,7 +7,8 @@ host = getpass.getuser()
 if not os.path.exists(r"C:\Users\{}\Desktop\ImageToPDF".format(host)):
     os.mkdir(r"C:\Users\{}\Desktop\ImageToPDF".format(host)) # creates a folder on the desktop that will hold your images
 print(input("Put your images in the ImageToPDF folder on your desktop! Press ENTER to continue."))
-imageList = [(Image.open(r"C:\Users\{}\Desktop\ImageToPDF\{}".format(host, n))).convert('RGB') for n in os.listdir(r"C:\Users\{}\Desktop\ImageToPDF".format(host))]
+Ospath = sorted(os.listdir(r"C:\Users\{}\Desktop\ImageToPDF".format(host)))
+imageList = [(Image.open(r"C:\Users\{}\Desktop\ImageToPDF\{}".format(host, n))).convert('RGB') for n in Ospath]
 # list comprehension that creates a list that holds all the paths of all the images
 imageList[0].save(r"C:\Users\{}\Desktop\ImageToPDF\Images.pdf".format(host), "PDF", resolution=100.0, save_all=True, append_images = imageList[1:]) # saves the image list into one pdf
 
