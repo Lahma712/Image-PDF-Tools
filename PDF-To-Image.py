6f95b26beb756340299e666a22e2176b31066167
Lahr Max <52128659+Lahma712@users.noreply.github.com>
2020-04-17 16:29:56 +0200

    Update PDF-To-Image.py

--- PDF-To-Image.py
+++ PDF-To-Image.py
@@ -3,10 +3,10 @@ import getpass
 from pdf2image import convert_from_path
 host = getpass.getuser()
 Path = r"C:\Users\{}\Desktop\PDFtoImage".format(host)
-print(input("Put your PDF in the PDFtoImage folder on your desktop! Press ENTER to continue."))
 
 if not os.path.exists(r"C:\Users\{}\Desktop\PDFtoImage".format(host)):
     os.mkdir(Path.format(host)) # creates a folder on the desktop that will hold your pdf
+print(input("Put your PDF in the PDFtoImage folder on your desktop! Press ENTER to continue."))
 
 pages = list([n for n in input("Page range (f.ex '1-3'): ").split('-')]) #list that holds the first and last page number
 pngList = convert_from_path(Path + "\{}".format(os.listdir(Path.format(host))[0]), first_page= int(pages[0]), last_page= int(pages[1])) #list of converted png files
